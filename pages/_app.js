530fc57b32aa333e143928e1ee8de9b542b2b4b1
ganesham019 <ganesham019@gmail.com>
2023-09-15 16:39:59 +0530

    added build file

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import Layout from "../components/Layout";
 import '../styles/global.css';
 import '../styles/index.scss';
 import { Inter_Tight } from 'next/font/google';
+
 const font = Inter_Tight({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }) {
   return (
     <Layout>

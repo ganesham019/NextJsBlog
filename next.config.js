436e0fed7ae0a75ec514714f75f8136e43db6f4e
ganesham019 <ganesham019@gmail.com>
2023-12-01 13:07:16 +0530

    refactor: updating the next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
+var BrotliPlugin = require('brotli-webpack-plugin');
+
 const nextConfig = {
   reactStrictMode: true,
-
+}
+module.exports = {
+  plugins: [
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
 }
 
 module.exports = nextConfig
 
-// const path = require('path')
- 
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'styles')],
-//   },
-// }
